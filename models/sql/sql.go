dfa4241eff493ae45ae3e25bb19dfa97b9b62b2f
Andreas Bielawski <brawl@nyanya.de>
2022-04-13 21:25:08 +0200

    Add ENV vars to enable MySQL TLS and ignore migrations

--- models/sql/sql.go
+++ models/sql/sql.go
@@ -22,14 +22,19 @@ func New() (*sqlx.DB, error) {
 	user := strings.TrimSpace(os.Getenv("MYSQL_USER"))
 	password := strings.TrimSpace(os.Getenv("MYSQL_PASSWORD"))
 	dbname := strings.TrimSpace(os.Getenv("MYSQL_DB"))
+	tls := strings.TrimSpace(os.Getenv("MYSQL_TLS"))
+	if tls == "" {
+		tls = "false"
+	}
 
 	connectionString := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=%s",
 		user,
 		password,
 		host,
 		port,
 		dbname,
+		tls,
 	)
 
 	db, err := sqlx.Connect("mysql", connectionString)
@@ -37,10 +42,13 @@ func New() (*sqlx.DB, error) {
 		return nil, err
 	}
 
-	migrationSource := &migrate.EmbedFileSystemMigrationSource{FileSystem: embeddedMigrations, Root: "migrations"}
-	_, err = migrate.Exec(db.DB, "mysql", migrationSource, migrate.Up)
-	if err != nil {
-		return nil, err
+	_, ignoreMigration := os.LookupEnv("IGNORE_SQL_MIGRATION")
+	if !ignoreMigration {
+		migrationSource := &migrate.EmbedFileSystemMigrationSource{FileSystem: embeddedMigrations, Root: "migrations"}
+		_, err = migrate.Exec(db.DB, "mysql", migrationSource, migrate.Up)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	db = db.Unsafe()
