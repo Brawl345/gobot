398ed571c6b1f3f13b8f3e3489f2b4909203606f
Andreas Bielawski <brawl@nyanya.de>
2022-05-06 23:34:37 +0200

    Add index to birthday_notifications_enabled

--- models/sql/migrations/0012-birthdays.sql
+++ models/sql/migrations/0012-birthdays.sql
@@ -4,4 +4,5 @@ ALTER TABLE `users`
     ADD `birthday` DATE NULL;
 
 ALTER TABLE `chats`
-    ADD `birthday_notifications_enabled` TINYINT(1) NOT NULL DEFAULT '0';
+    ADD `birthday_notifications_enabled` TINYINT(1) NOT NULL DEFAULT '0',
+    ADD INDEX `birthday_notifications_enabled` (`birthday_notifications_enabled`);
