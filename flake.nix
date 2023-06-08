bba6f79783c59f04c4a39319d7206e6c794d08ba
Andreas Bielawski <brawl@nyanya.de>
2023-06-08 13:46:04 +0200

    nix-flake: Remove flake-utils and add golangci-lint

--- flake.nix
+++ flake.nix
@@ -3,48 +3,65 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, ... }:
+  outputs = { self, nixpkgs, ... }:
 
     let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
+
       version =
         if (self ? shortRev)
         then self.shortRev
         else "dev";
     in
+    {
 
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [ pkgs.go ];
-          shellHook = ''
-            export PRINT_MSGS=1
-            export PRETTY_PRINT_LOG=1
-            export DEBUG=1
-          '';
-        };
-        packages.default = pkgs.buildGoModule {
-          pname = "gobot";
-          inherit version;
-          src = pkgs.lib.cleanSource self;
-
-          # Update the hash if go dependencies change!
-          # vendorHash = pkgs.lib.fakeHash;
-          vendorHash = "sha256-xc6BBLCwhJ9wF7ecNDZCaXI1puDb8BB5rLxD3o41Pgo=";
-
-          ldflags = [ "-s" "-w" ];
-
-          meta = {
-            description = "Multi-purpose Telegram bot";
-            homepage = "https://github.com/Brawl345/gobot";
-            license = pkgs.lib.licenses.unlicense;
-            platforms = pkgs.lib.platforms.darwin ++ pkgs.lib.platforms.linux;
+      devShells = forAllSystems
+        (pkgs: {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.go
+              pkgs.golangci-lint
+            ];
+            shellHook = ''
+              export PRINT_MSGS=1
+              export PRETTY_PRINT_LOG=1
+              export DEBUG=1
+            '';
           };
-        };
-      });
+        });
+
+
+      packages = forAllSystems
+        (pkgs: {
+          default =
+            pkgs.buildGoModule
+              {
+                pname = "gobot";
+                inherit version;
+                src = pkgs.lib.cleanSource self;
+
+                # Update the hash if go dependencies change!
+                # vendorHash = pkgs.lib.fakeHash;
+                vendorHash = "sha256-xc6BBLCwhJ9wF7ecNDZCaXI1puDb8BB5rLxD3o41Pgo=";
+
+                ldflags = [ "-s" "-w" ];
+
+                meta = {
+                  description = "Multi-purpose Telegram bot";
+                  homepage = "https://github.com/Brawl345/gobot";
+                  license = pkgs.lib.licenses.unlicense;
+                  platforms = pkgs.lib.platforms.darwin ++ pkgs.lib.platforms.linux;
+                };
+              };
+        });
+    };
 }
