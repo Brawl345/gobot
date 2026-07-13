e610e44daa5989197d6ab58556f78b50542197ac
Andreas Bielawski <brawl@nyanya.de>
2026-07-13 11:15:01 +0200

    brave_images: Fix copy-paste fail that cleaned the wrong table

--- model/sql/brave_images_cleanup.go
+++ model/sql/brave_images_cleanup.go
@@ -18,9 +18,9 @@ func NewBraveImagesCleanupService(db *sqlx.DB) *braveImagesCleanupService {
 }
 
 func (db *braveImagesCleanupService) Cleanup() error {
-	const query = `DELETE giq, b FROM google_images_queries giq
-   RIGHT JOIN brave_images b ON giq.id = b.query_id
-   WHERE giq.created_at < NOW() - INTERVAL 7 DAY`
+	const query = `DELETE biq, b FROM brave_images_queries biq
+   RIGHT JOIN brave_images b ON biq.id = b.query_id
+   WHERE biq.created_at < NOW() - INTERVAL 7 DAY`
 	_, err := db.Exec(query)
 	return err
 }
